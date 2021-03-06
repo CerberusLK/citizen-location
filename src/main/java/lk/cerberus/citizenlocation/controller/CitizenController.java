71fd07ae19355d1249522c7b4cc6d1499ab20dc2
Chathura Prabasha <chathuraprabasha@gmail.com>
2021-03-07 02:22:50 +0530

    XML support added for the service

--- src/main/java/lk/cerberus/citizenlocation/controller/CitizenController.java
+++ src/main/java/lk/cerberus/citizenlocation/controller/CitizenController.java
@@ -3,6 +3,7 @@ package lk.cerberus.citizenlocation.controller;
 import lk.cerberus.citizenlocation.entity.Citizen;
 import lk.cerberus.citizenlocation.service.CitizenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,28 +16,28 @@ public class CitizenController {
     @Autowired
     private CitizenService citizenService;
 
-    @GetMapping
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public List<Citizen> getAllCitizen() {
         return citizenService.getAllCitizen();
     }
 
-    @GetMapping("/{nic}")
+    @GetMapping(path = "/{nic}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public Optional<Citizen> getCitizenByNIC(@PathVariable(value = "nic") String nic) {
         return citizenService.getCitizenByNIC(nic);
     }
 
-    @PostMapping
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public void addCitizen(@RequestBody Citizen citizen) {
         citizenService.addCitizen(citizen);
     }
 
 
-    @PutMapping("/{nic}")
+    @PutMapping(path = "/{nic}", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public void updateCitizen(@PathVariable(value = "nic") String nic, @RequestBody Citizen citizen) {
         citizenService.updateCitizen(nic, citizen);
     }
 
-    @DeleteMapping("/{nic}")
+    @DeleteMapping(path = "/{nic}")
     public void deleteCitizen(@PathVariable(value = "nic") String nic) {
         citizenService.deleteCitizen(nic);
     }
--- src/main/java/lk/cerberus/citizenlocation/repository/CitizenRepository.java
+++ src/main/java/lk/cerberus/citizenlocation/repository/CitizenRepository.java
@@ -5,7 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CitizenRepository extends CrudRepository<Citizen, String> {
 
-//    Citizen findByNIC(String nic);
-//
-//    List<Citizen> findAll();
 }
